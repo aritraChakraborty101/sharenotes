4e64f9936573d124cd35e3f5053117db51406f5f
Aritra Chakraborty <aritra.chakraborty@g.bracu.ac.bd>
2026-02-27 14:33:40 +0600

    Started a the project

---
+++ src/main/java/com/aritra/sharenotes/SharenotesApplication.java
@@ -0,0 +1,13 @@
+package com.aritra.sharenotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharenotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharenotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aritra/sharenotes/SharenotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aritra.sharenotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharenotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
